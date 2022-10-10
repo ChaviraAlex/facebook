079d6dc76b36110e21a4945fe45441394140d610
ChaviraAlex <112144506+ChaviraAlex@users.noreply.github.com>
2022-10-10 14:15:07 -0500

    last_commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,15 @@
 import './NavBar.css'
-import photo from './../../public/images/photo.jpg'
-import logo from './../../public/images/facebook_logo.png'
-import home from './../../public/icons/svg/home-alt2.svg'
-import watch from './../../public/icons/svg/tv.svg'
-import store from './../../public/icons/svg/bag.svg'
-import groups from './../../public/icons/svg/people.svg'
-import gaming from './../../public/icons/svg/play.svg'
-import menu from './../../public/icons/svg/apps.svg'
-import chats from './../../public/icons/svg/chat.svg'
-import notify from './../../public/icons/svg/bell.svg'
+
+import photo from './../images/photo.jpg'
+import logo from './../images/facebook_logo.png'
+import home from './../icons/svg/home-alt2.svg'
+import watch from './../icons/svg/tv.svg'
+import store from './../icons/svg/bag.svg'
+import groups from './../icons/svg/people.svg'
+import gaming from './../icons/svg/play.svg'
+import menu from './../icons/svg/apps.svg'
+import chats from './../icons/svg/chat.svg'
+import notify from './../icons/svg/bell.svg'
 
 function NavBar (){
    return (
